5327812dd41e61a1a4d1395008b68c811603b35a
pt <pt_liangyue@outlook.com>
2020-06-20 20:31:33 +0800

    正交摄像机，立方体

--- index.js
+++ index.js
@@ -22,8 +22,12 @@ const scene = new THREE.Scene()
 // scene.add(cube);
 //
 // 正交投影摄像机
-const camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 1, 10);
-camera.position.set(0, 0, 5);
+// THREE.OrthographicCamera(left, right, top, bottom, near, far)
+const camera = new THREE.OrthographicCamera(-1, 3, 1.5, -1.5, 1, 10);
+camera.position.set(4, -3, 5);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+// 不过一定要注意，lookAt函数接受的是一个THREE.Vector3的实例，
+// 因此千万别写成camera.lookAt(0, 0, 0)，否则非但不能得到理想的效果，而且不会报错，使你很难找到问题所在。
 scene.add(camera);
 
 // 原点处创建一个边长为1的正方体，为了和透视效果做对比，这里我们使用wireframe而不是实心的材质，以便看到正方体后方的边
